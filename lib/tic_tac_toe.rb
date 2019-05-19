aa364db2a1804fe2f5a560c924c2724705e716a5
mathematical-algorithm-3478 <jlboyer1@gmail.com>
2019-05-19 20:52:32 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -31,11 +31,11 @@ class TicTacToe
   end
   
   def position_taken?(index)
-    @board[index] != " " && @board[index] != ""
+    @board[index] != " " && @board[index] = ""
   end
   
-   def valid_move?(index)
-  index.between?(0,8) && !position_taken?(index)
+  def valid_move?(index)
+    index.between?(0,8) && !position_taken?(index)
   end
   
   def turn
